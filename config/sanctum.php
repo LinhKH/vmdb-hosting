c61a1ad67b879ebc67b5d8ac933cb3e2c2554ee6
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-12-15 17:52:48 +0700

    test

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'dev-vmdb-re.gw-dev.jp,localhost,localhost:8000,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'news-portal.shop,https://news-portal.shop,localhost,localhost:8000,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
